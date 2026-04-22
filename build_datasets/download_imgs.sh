db9fd62f79447604d17becd81e502964c31994ad
xh <xiehui03@qq.com>
2026-04-22 23:07:38 +0800

    build: update dependencies and dataset download script
    - Add nvitop, modelscope, and huggingface_hub to requirements
    - Update download_imgs.sh to handle new dataset paths and unzip
    - Reorder training commands in script.sh for better organization

--- build_datasets/download_imgs.sh
+++ build_datasets/download_imgs.sh
@@ -1,1 +1,5 @@
-modelscope download --dataset leehirwin0215/VisualGenome images.zip --local_dir ./sample_data
+modelscope download --dataset leehirwin0215/VisualGenome images.zip --local_dir ./data
+
+hf download --repo-type dataset jn12/VisualGenome images.zip --local-dir ./build_datasets/data/VisualGenome
+
+unzip ./build_datasets/data/VisualGenome/images.zip -d ./build_datasets/data/VG_100K
